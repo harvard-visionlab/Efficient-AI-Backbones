aa3ff55441951ca1bee6b01950b57cacdb9736ef
grez72 <grez72@gmail.com>
2025-01-28 14:55:57 +0000

    fix bug

--- wavemlp_pytorch/models/wavemlp.py
+++ wavemlp_pytorch/models/wavemlp.py
@@ -30,7 +30,7 @@ import torch.nn as nn
 from timm.data import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
 from timm.models.layers import DropPath, trunc_normal_
 from timm.models.registry import register_model
-from timm.models.layers.helpers import to_2tuple
+from timm.layers.helpers import to_2tuple
 
 import math
 from torch import Tensor
